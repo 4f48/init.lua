276bd68fe48220594502529df76cc8cc5212bb42
0x4f48 <0x4f48@proton.me>
2024-01-03 22:12:50 +0100

    Add LSP, snippets, neodev.nvim

--- lua/4f48/lazy.lua
+++ lua/4f48/lazy.lua
@@ -1,16 +1,15 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+	  vim.fn.system({
+	    "git",
+	    "clone",
+	    "--filter=blob:none",
+	    "https://github.com/folke/lazy.nvim.git",
+	    "--branch=stable",
+	    lazypath,
+	  })
+	end
+	vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
@@ -19,5 +18,26 @@ require("lazy").setup({
             		"nvim-lua/plenary.nvim",
         	},
     	},
+	{ "neovim/nvim-lspconfig",
+		dependencies = {
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+		}
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		build = "make install_jsregexp",
+	},
+	{ "folke/neodev.nvim", opts = {} },
+	{ "williamboman/mason.nvim" },
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 })
--- lua/4f48/setup.lua
+++ lua/4f48/setup.lua
@@ -1,1 +1,27 @@
+local cmp = require("cmp")
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+
+require("mason-lspconfig").setup_handlers {
+	function (server_name)
+	    	require("lspconfig")[server_name].setup {}
+		end,
+	}
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	sources = cmp.config.sources ({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	},
+	{
+		{ name = "buffer" },
+	}),
+})
+
 vim.cmd.colorscheme "catppuccin"
